f6cc40019fd7f45b66603f031e018755ce604cf2
nkrumahthis <nkrumahthis@gmail.com>
2020-12-15 10:24:47 +0000

    dev alarm functionality

--- main.py
+++ main.py
@@ -1,34 +1,48 @@
 import os
-import time
-import shutil
+import threading
 import tkinter as tk
+import time
+from datetime import datetime
+from playsound import playsound
+import pyttsx3
+from pygame import mixer
+import csv
 
-import gui.ClockWindow as clockwindow
-
-
-def listTunes(path):
-    tunes = []
-    for(dirpath, dirnames, filenames) in os.walk(path):
-        tunes.extend(filenames)
-        break
+tunes_path = ""
 
 
 def initialize():
     # get current working directory
     path = os.getcwd()
-    tunes_path = path + "/alarm_tunes"
+    tunes_path = path + "\\alarm_tunes"
 
     # if no directory is present, create one
     if not os.path.isdir(tunes_path):
         print("no alarm sounds directory, creating one")
         os.makedirs(tunes_path)
 
-    listTunes(tunes_path)
+
+def target():
+    alarmtime = "10:17"
+    initialize()
+    engine = pyttsx3.init()
+    mixer.init()
+    while True:
+        localtime = datetime.now().strftime("%H:%M")
+        if localtime == alarmtime:
+            mixer.music.load("alarm_tunes\\al1.mp3")
+            mixer.music.play()
+            engine.say("breakfast time")
+            engine.runAndWait()
+            time.sleep(60)
+
+            break
 
 
 def main():
     initialize()
-    clockwindow.show()
+    mainthread = threading.Thread(target=target)
+    mainthread.start()
 
 
 if __name__ == "__main__":
